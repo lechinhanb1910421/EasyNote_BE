0c636353336fbbd8019ade7e87d568d771f704dc
Nhan Le <lechinhan012@gmail.com>
2022-10-29 09:06:00 +0700

    Change server Port to my reference

--- app/config/index.js
+++ app/config/index.js
@@ -1,10 +1,10 @@
 const config = {
   app: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 2702
   },
   db: {
-    uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/todoapp",
-  },
-};
+    uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/todoapp'
+  }
+}
 
-module.exports = config;
+module.exports = config
